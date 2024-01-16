341f947881652befb1dd62ea6e8eba46fbba778e
Javier <javierpd45@gmail.com>
2024-01-15 22:04:24 -0400

    Agregadas carpetas de Behavior y Exception

---
+++ backend/src/Core/Hospital.Application/Exceptions/BadRequestException.cs
@@ -0,0 +1,8 @@
+namespace Hospital.Application.Exceptions;
+
+public class BadRequestException : ApplicationException
+{
+    public BadRequestException(string message): base(message)
+    {
+    }
+}
